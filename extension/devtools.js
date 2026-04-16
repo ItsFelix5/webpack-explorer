ea1076e1ee3f5a5a4d27cb3b303a41a5bf2bd08b
itsFelix5 <60808107+ItsFelix5@users.noreply.github.com>
2026-04-16 18:56:47 +0200

    Extension

---
+++ extension/devtools.js
@@ -0,0 +1,6 @@
+chrome.devtools.panels.create(
+  "Webpack Explorer",
+  "",
+  "index.html",
+  function (panel) {},
+);
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,18 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
+import { cpSync } from "fs";
 
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [
+    svelte(),
+    {
+      name: "cp",
+      writeBundle: () => cpSync("extension", "dist", { recursive: true }),
+    },
+  ],
   build: {
     rollupOptions: {
-      input: "panel.html",
+      input: "index.html",
     },
   },
 });
